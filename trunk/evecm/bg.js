13d31bf271f78184dfade8c168958316d1b4ae32
Leshiy.S <Leshiy.S@bd205875-d2c7-f3c9-70fa-fd22e6c557a3>
2010-12-07 15:52:00 +0000

    added skillsumms on group headers

--- trunk/evecm/bg.js
+++ trunk/evecm/bg.js
@@ -1,114 +1,111 @@
-var compteur =[];
+var compteur = [];
 var digit_del = ",";
 var tabcouleurfind = new Array();
-            var apiserver = "http://api.eve-online.com";
+var apiserver = "http://api.eve-online.com";
 
-            var msPerDay = 24 * 60 * 60 * 1000;
-            var msPerHour = 60 * 60 * 1000;
-            var msPerMinutes =  60 * 1000;
-            var msPerSeconds = 1000;
-            var dateFin;
-            var currSkill;
-            var queue = [];
-            var trLevel;
-            var divTimeLeft;
+var msPerDay = 24 * 60 * 60 * 1000;
+var msPerHour = 60 * 60 * 1000;
+var msPerMinutes = 60 * 1000;
+var msPerSeconds = 1000;
+var dateFin;
+var currSkill;
+var queue = [];
+var trLevel;
+var divTimeLeft;
 
-            var reqCharacterSheet = new XMLHttpRequest();
-            var reqSkillInTraining = new XMLHttpRequest();
-            var skillTreeRequest = new XMLHttpRequest();
-            var skillQueue = new XMLHttpRequest();
-            var apikey = localStorage["apikey"];
-            var userid = localStorage["userid"];
-            var characterid = localStorage["characterid"];
+var reqCharacterSheet = new XMLHttpRequest();
+var reqSkillInTraining = new XMLHttpRequest();
+var skillTreeRequest = new XMLHttpRequest();
+var skillQueue = new XMLHttpRequest();
+var apikey = localStorage["apikey"];
+var userid = localStorage["userid"];
+var characterid = localStorage["characterid"];
 
-            function init() {
-                skillTreeRequest.open("GET","skillTree.xml",false);
-                skillTreeRequest.send("");
-                reqSkillInTraining.open(
-                    "GET",
-                    apiserver + "/char/SkillInTraining.xml.aspx?userID=" + userid + "&characterID=" + characterid + "&apiKey=" + apikey,
-                true);
-                reqSkillInTraining.onload = recupSkillInTraining;
-                reqSkillInTraining.send(null);
-                reqCharacterSheet.open(
-                 "GET",
-                    apiserver + "/char/CharacterSheet.xml.aspx?userID=" + userid + "&characterID=" + characterid + "&apiKey=" + apikey,
-                true);
-                reqCharacterSheet.onload = recupInfosPerso;
-                reqCharacterSheet.send(null);
+function init() {
+    skillTreeRequest.open("GET", "skillTree.xml", false);
+    skillTreeRequest.send("");
+    reqSkillInTraining.open("GET", apiserver + "/char/SkillInTraining.xml.aspx?userID=" + userid + "&characterID=" + characterid + "&apiKey=" + apikey, true);
+    reqSkillInTraining.onload = recupSkillInTraining;
+    reqSkillInTraining.send(null);
+    reqCharacterSheet.open("GET", apiserver + "/char/CharacterSheet.xml.aspx?userID=" + userid + "&characterID=" + characterid + "&apiKey=" + apikey, true);
+    reqCharacterSheet.onload = recupInfosPerso;
+    reqCharacterSheet.send(null);
+}
 
+function refreshDateFin() {
+    compteur = differenceDates(queue[queue.length - 1][3]);
+    if (differenceDates(dateFin)[3] <= 0) {
+        var notification = webkitNotifications.createNotification('icon.png', // icon url - can be relative
+        'Skill training complete', // notification title
+        currSkill + ' ' + trLevel // notification body text
+        );
+        notification.show();
+        location.reload();
+    }
+    chrome.browserAction.setBadgeText({
+        text: compteur[0]
+    });
+    chrome.browserAction.setTitle({
+        title: drawQueue()
+    });
+    chrome.browserAction.setBadgeBackgroundColor({
+        color: compteur[2]
+    });
+    setTimeout("refreshDateFin()", 1000)
+}
 
-            }
+function drawQueue() {
+    var ret = '';
+    for (var i = 0; i < queue.length; i++) {
+        ret = ret + queue[i][0] + ' ' + queue[i][1] + ': ' + differenceDates(queue[i][3])[1] + '\n';
+    }
+    var ret2 = ret.slice(0, -1);
+    return ret2;
+}
 
-            function refreshDateFin() {
-                compteur = differenceDates(queue[queue.length-1][3]);
-                if (differenceDates(dateFin)[3]<=0) {
-                    var notification = webkitNotifications.createNotification(
-                    'icon.png',  // icon url - can be relative
-                    'Skill training complete',  // notification title
-                    currSkill+' '+trLevel  // notification body text
-                     );
-                notification.show();
-                location.reload();
-                }
-                chrome.browserAction.setBadgeText({text:compteur[0]});
-                chrome.browserAction.setTitle({title:drawQueue()});
-                chrome.browserAction.setBadgeBackgroundColor({color:compteur[2]});
-                setTimeout("refreshDateFin()",1000)
-            }
+function queueCalc() {
+    var skills = skillQueue.responseXML.getElementsByTagName("row");
+    for (var i = 0; i < skills.length; i++) {
+        queue[i] = [];
+        queue[i][0] = getLibelleSkill(skills[i].getAttribute('typeID'))[0];
+        queue[i][1] = toRoman(skills[i].getAttribute('level') + ' ');
+        queue[i][2] = Date.parse(skills[i].getAttribute('startTime'));
+        queue[i][2].addMinutes(new Date().getTimezoneOffset() * -1);
+        queue[i][3] = Date.parse(skills[i].getAttribute('endTime'));
+        queue[i][3].addMinutes(new Date().getTimezoneOffset() * -1);
+    }
+    refreshDateFin();
+}
 
-            function drawQueue() {
-                var ret = '';
-                for (var i=0; i<queue.length; i++) {
-                    ret = ret+queue[i][0]+' '+queue[i][1]+': '+differenceDates(queue[i][3])[1]+'\n';
-                }
-                var ret2 = ret.slice(0,-1);
-                return ret2;
-            }
-
-            function queueCalc() {
-                var skills = skillQueue.responseXML.getElementsByTagName("row");
-                for (var i=0; i<skills.length; i++) {
-                    queue[i]=[];
-                    queue[i][0] = getLibelleSkill(skills[i].getAttribute('typeID'))[0];
-                    queue[i][1] = toRoman(skills[i].getAttribute('level')+' ');
-                    queue[i][2] = Date.parse(skills[i].getAttribute('startTime'));
-                    queue[i][2].addMinutes(new Date().getTimezoneOffset() * -1);
-                    queue[i][3] = Date.parse(skills[i].getAttribute('endTime'));
-                    queue[i][3].addMinutes(new Date().getTimezoneOffset() * -1);
-                }
-                refreshDateFin();
-
-            }
-
-            function recupSkillInTraining() {
-                 var trainingTypeIDList = reqSkillInTraining.responseXML.getElementsByTagName("trainingTypeID");
-                var trainingToLevelList = reqSkillInTraining.responseXML.getElementsByTagName("trainingToLevel");
-                var trainingEndTimeList = reqSkillInTraining.responseXML.getElementsByTagName("trainingEndTime");
-                 var trainingBool = reqSkillInTraining.responseXML.getElementsByTagName("skillInTraining")[0].textContent;
-                 if (trainingBool==1) {
-                    var trainingTypeIDElement = trainingTypeIDList[0];
-                    var trainingToLevelElement = trainingToLevelList[0];
-                    var trainingEndTimeElement = trainingEndTimeList[0];
-                    trLevel = toRoman(trainingToLevelElement.textContent+" ");
+function recupSkillInTraining() {
+    var trainingTypeIDList = reqSkillInTraining.responseXML.getElementsByTagName("trainingTypeID");
+    var trainingToLevelList = reqSkillInTraining.responseXML.getElementsByTagName("trainingToLevel");
+    var trainingEndTimeList = reqSkillInTraining.responseXML.getElementsByTagName("trainingEndTime");
+    var trainingBool = reqSkillInTraining.responseXML.getElementsByTagName("skillInTraining")[0].textContent;
+    if (trainingBool == 1) {
+        var trainingTypeIDElement = trainingTypeIDList[0];
+        var trainingToLevelElement = trainingToLevelList[0];
+        var trainingEndTimeElement = trainingEndTimeList[0];
+        trLevel = toRoman(trainingToLevelElement.textContent + " ");
 
 
-                    currSkill = getLibelleSkill(trainingTypeIDElement.textContent)[0];
-                    var trainingEndTime = trainingEndTimeElement.textContent;
-                    dateFin = Date.parse(trainingEndTime);
-                    dateFin.addMinutes(new Date().getTimezoneOffset() * -1);
-                    skillQueue.open(
-                        "GET",
-                        apiserver + "/char/SkillQueue.xml.aspx?userID=" + userid + "&characterID=" + characterid + "&apiKey=" + apikey,
-                    true);
-                        skillQueue.onload = queueCalc;
-                        skillQueue.send(null);
-                 } else{
-                     alert("Warning! No Skill in training!");
-                     chrome.browserAction.setBadgeText({text:"warn"});
-                     chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,255]});
-                 }
-           }
+        currSkill = getLibelleSkill(trainingTypeIDElement.textContent)[0];
+        var trainingEndTime = trainingEndTimeElement.textContent;
+        dateFin = Date.parse(trainingEndTime);
+        dateFin.addMinutes(new Date().getTimezoneOffset() * -1);
+        skillQueue.open("GET", apiserver + "/char/SkillQueue.xml.aspx?userID=" + userid + "&characterID=" + characterid + "&apiKey=" + apikey, true);
+        skillQueue.onload = queueCalc;
+        skillQueue.send(null);
+    } else {
+        alert("Warning! No Skill in training!");
+        chrome.browserAction.setBadgeText({
+            text: "warn"
+        });
+        chrome.browserAction.setBadgeBackgroundColor({
+            color: [255, 0, 0, 255]
+        });
+    }
+}
 
 
 
@@ -116,13 +113,13 @@ function differenceDates(d) {
     var dateNow = new Date();
     var stop = 0;
     var ret = [];
-    ret[1]="";
+    ret[1] = "";
     var timeBetween = d.valueOf() - dateNow.valueOf();
     ret[3] = timeBetween;
     var jours = Math.floor(timeBetween / msPerDay);
-    if (jours > 0){
+    if (jours > 0) {
         timeBetween = timeBetween - (jours * msPerDay);
-        ret[0] = jours+"d";
+        ret[0] = jours + "d";
         ret[1] = jours + " days, ";
         ret[2] = [30, 60, 150, 230];
         stop = 1;
@@ -131,20 +128,20 @@ function differenceDates(d) {
     var heures = Math.floor(timeBetween / msPerHour);
     if (heures > 0) {
         timeBetween = timeBetween - (heures * msPerHour);
-        ret[1] = ret[1]+heures + " hours, ";
-        if (stop == 0){
-            ret[0] =  heures+"h";
+        ret[1] = ret[1] + heures + " hours, ";
+        if (stop == 0) {
+            ret[0] = heures + "h";
             ret[2] = [255, 20, 20, 230];
             stop = 1;
         }
     }
 
     var minutes = Math.floor(timeBetween / msPerMinutes);
-    if (minutes > 0){
+    if (minutes > 0) {
         timeBetween = timeBetween - (minutes * msPerMinutes);
-        ret[1] = ret[1]+minutes + " mins, "
+        ret[1] = ret[1] + minutes + " mins, "
         if (stop == 0) {
-            ret[0] = minutes+"m";
+            ret[0] = minutes + "m";
             ret[2] = [255, 20, 20, 230];
             stop = 1;
         }
@@ -153,9 +150,9 @@ function differenceDates(d) {
     var secondes = Math.floor(timeBetween / msPerSeconds);
     if (secondes > 0) {
         timeBetween = timeBetween - (secondes * msPerSeconds);
-        ret[1] = ret[1]+secondes + " sec"
+        ret[1] = ret[1] + secondes + " sec"
         if (stop == 0) {
-            ret[0] = secondes+"s";
+            ret[0] = secondes + "s";
             ret[2] = [255, 20, 20, 230];
         }
     }
@@ -166,7 +163,7 @@ function differenceDates(d) {
 function getLibelleSkill(idSkill) {
     var skillTreeDoc = skillTreeRequest.responseXML;
     var currentTypes = skillTreeDoc.getElementsByTagName("row");
-    var ret=[];
+    var ret = [];
 
     for (var i = 0, row; row = currentTypes[i]; i++) {
         if (row.getAttribute("typeID") == idSkill && row.getAttribute("typeName") != null) {
@@ -179,22 +176,22 @@ function getLibelleSkill(idSkill) {
 
 function toRoman(d) {
     switch (d) {
-                       case (d='1 '):
-                           return 'I';
-                       break
-                       case (d='2 '):
-                           return 'II';
-                       break
-                       case (d='3 '):
-                           return 'III';
-                       break
-                       case (d='4 '):
-                           return 'IV';
-                       break
-                       case (d='5 '):
-                            return 'V';
-                       break
-                   }
+    case (d = '1 '):
+        return 'I';
+        break
+    case (d = '2 '):
+        return 'II';
+        break
+    case (d = '3 '):
+        return 'III';
+        break
+    case (d = '4 '):
+        return 'IV';
+        break
+    case (d = '5 '):
+        return 'V';
+        break
+    }
 }
 
 
@@ -218,14 +215,16 @@ function recupInfosPerso() {
     var groups = skillTreeDoc.getElementsByTagName("row");
 
     for (var i = 0, row; row = groups[i]; i++) {
-        if (row.getAttribute("groupName") != null && row.getAttribute("groupID")!= null) {
+        if (row.getAttribute("groupName") != null && row.getAttribute("groupID") != null) {
             var tableGroup = document.createElement("table");
-            tableGroup.setAttribute("idGroup",row.getAttribute("groupID"));
+            tableGroup.setAttribute("idGroup", row.getAttribute("groupID"));
             var trHeaderGroup = document.createElement("tr");
             var thHeaderGroup = document.createElement("th");
-            thHeaderGroup.setAttribute("colspan",2);
-            thHeaderGroup.className ="header";
+            thHeaderGroup.setAttribute("colspan", 2);
+            thHeaderGroup.className = "header";
             thHeaderGroup.innerText = row.getAttribute("groupName");
+            tableGroup.setAttribute('spcount',0);
+            tableGroup.setAttribute('skillcount',0);
             trHeaderGroup.appendChild(thHeaderGroup);
             tableGroup.appendChild(trHeaderGroup);
             listGroup.push(tableGroup);
@@ -248,16 +247,16 @@ function recupInfosPerso() {
     var skillList = rowsetSkillsElement.getElementsByTagName("row");
     var skillArr = [];
     for (var i = 0, row; row = skillList[i]; i++) {
-        skillArr[i]=[];
-        skillArr[i][1]=row.getAttribute("typeID");
-        skillArr[i][0]=getLibelleSkill(row.getAttribute("typeID"))[0];
-        skillArr[i][3]=getLibelleSkill(row.getAttribute("typeID"))[1];
-        skillArr[i][2]=row.getAttribute("level");
-        skillArr[i][4]=row.getAttribute("skillpoints");
+        skillArr[i] = [];
+        skillArr[i][1] = row.getAttribute("typeID");
+        skillArr[i][0] = getLibelleSkill(row.getAttribute("typeID"))[0];
+        skillArr[i][3] = getLibelleSkill(row.getAttribute("typeID"))[1];
+        skillArr[i][2] = row.getAttribute("level");
+        skillArr[i][4] = row.getAttribute("skillpoints");
     }
     skillArr.sort();
     for (var i = 0, row; row = skillArr[i]; i++) {
-      
+
         var trSkill = document.createElement("tr");
         var lvImg = document.createElement('img');
         var tdSkill = document.createElement("td");
@@ -271,22 +270,29 @@ function recupInfosPerso() {
         trSkill.className = 'skill nd';
         tdSkill.className = 'skillName';
         tdLevel.className = 'skillLv';
-        trSkill.setAttribute("idSkill",row[1]);
-        if (row[2]==5) {
-            skillName.innerHTML += row[0]+'('+row[3]+'x)<br>SP: '+delim(row[4]);
+        trSkill.setAttribute("idSkill", row[1]);
+        trSkill.setAttribute("spCount", row[4]);
+        if (row[2] == 5) {
+            skillName.innerHTML += row[0] + '(' + row[3] + 'x)<br>SP: ' + delim(row[4]);
         } else {
-            skillName.innerHTML += row[0]+'('+row[3]+'x)<br>SP: '+delim(row[4])+'/'+delim(getMaxSP(row[2],row[3]));
+            skillName.innerHTML += row[0] + '(' + row[3] + 'x)<br>SP: ' + delim(row[4]) + '/' + delim(getMaxSP(row[2], row[3]));
         }
         tdSkill.appendChild(skillName);
-        lvImg.setAttribute('src', 'img/level'+row[2]+'.gif');
+        lvImg.setAttribute('src', 'img/level' + row[2] + '.gif');
         trSkill.appendChild(tdSkill);
         trSkill.appendChild(tdLevel);
         tdLevel.appendChild(lvImg);
-
-        placeSkillInTableGroup(trSkill,listGroup);
-        //tableSkills.appendChild(trSkill);
+        placeSkillInTableGroup(trSkill, listGroup);
+        
     }
-
+    for (var i = 0, row; row = listGroup[i]; i++) {
+        var sp = row.getAttribute('spCount');
+        var skills = row.getAttribute('skillCount');
+        var thSpan = document.createElement('span');
+        thSpan.innerHTML = ' skills: '+delim(skills)+' SP: '+delim(sp);
+        thSpan.className = 'spsumm';
+        row.getElementsByTagName('th')[0].appendChild(thSpan);
+    }
 
     document.getElementById("idName").innerText = name;
     document.getElementById("idBalance").innerHTML = balance;
@@ -296,16 +302,21 @@ function recupInfosPerso() {
             document.getElementById("idListeSkillsConnus").appendChild(row);
         }
     }
-
-
 }
 
 
-function placeSkillInTableGroup(trSkill,listGroup) {
+function placeSkillInTableGroup(trSkill, listGroup) {
     var idGroup = getGroupIDBySkillID(trSkill.getAttribute("idSkill"));
+    var sp = parseInt(trSkill.getAttribute("spCount"));
     for (var i = 0, row; row = listGroup[i]; i++) {
         if (row.getAttribute("idGroup") == idGroup) {
             row.appendChild(trSkill);
+            var lastSp = parseInt(row.getAttribute('spCount'));
+            var currSp = lastSp+sp;
+            row.setAttribute('spCount',currSp.toString());
+            var lastSkills = parseInt(row.getAttribute('skillCount'));
+            var currSkills = lastSkills+1;
+            row.setAttribute('skillCount',currSkills.toString());
         }
     }
 }
@@ -321,10 +332,10 @@ function getGroupIDBySkillID(idSkill) {
 }
 
 function delim(st) {
-    var trSt = st+'';
-    return trSt.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1'+digit_del);
+    var trSt = st + '';
+    return trSt.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1' + digit_del);
 }
 
-function getMaxSP(lv,rank) {
-   return  Math.ceil(Math.pow(2,((2.5*(parseInt(lv)+1))-2.5))*250*rank);
+function getMaxSP(lv, rank) {
+    return Math.ceil(Math.pow(2, ((2.5 * (parseInt(lv) + 1)) - 2.5)) * 250 * rank);
 }
